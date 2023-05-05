94101b57d11f1639da62345a0f266d7716a6143d
euliveiras <matheus.skm@hotmail.com>
2023-05-05 18:31:45 -0300

    feat: created makeVehicle factory fn

---
+++ test/factories/makeVehicle.ts
@@ -0,0 +1,15 @@
+import type { VehicleProps } from "domain/entities/Vehicle";
+import { Vehicle } from "domain/entities/Vehicle";
+
+type OverrideProps = Partial<VehicleProps>;
+
+export function makeVehicle(override: OverrideProps) {
+    return new Vehicle({
+        driverId: "some-driver-id",
+        id: "some-id",
+        licensePlate: "some-license-plate",
+        proprietaryId: "some-proprietary-id",
+        vehicleType: "some-type",
+        ...override,
+    });
+}
