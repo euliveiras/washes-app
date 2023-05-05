6c4b4067403d288dc47c2fe1d9bbeec40bcb1c76
euliveiras <matheus.skm@hotmail.com>
2023-05-04 22:07:23 -0300

    feat: created validateName method

--- domain/entities/Driver.ts
+++ domain/entities/Driver.ts
@@ -1,14 +1,46 @@
-// >> telefone, nome, id do veículo (pode ter mais de um caminhão)
-
 interface DriverProps {
-name: String;
-id: String[];
-
+    id: String;
+    name: String;
+    vehicleIds: String[];
+    phones: Number[];
 }
 
 export class Driver {
-	private _props: DriverProps;
-	constructor(props: DriverProps){
-		this._props = props
-	}
+    private _props: DriverProps;
+    constructor(props: DriverProps) {
+		this.validateName(props.name)
+        this._props = props;
+    }
+
+    private validateName(name: String) {
+        if (name.length < 3) {
+            throw new Error("Name must have more than 2 words");
+        }
+
+        return true;
+    }
+
+    public get name() {
+        return this._props.name;
+    }
+
+    private set name(name) {
+        this._props.name = name;
+    }
+
+    public get vehicleIds() {
+        return this._props.vehicleIds;
+    }
+
+    private set vehicleIds(ids: String[]) {
+        this._props.vehicleIds = ids;
+    }
+
+    public get phones() {
+        return this._props.phones;
+    }
+
+    private set phones(phones: Number[]) {
+        this._props.phones = phones;
+    }
 }
