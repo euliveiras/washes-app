d44c1db75083d89f21f3aee9a29ade95b3a2ef6c
euliveiras <matheus.skm@hotmail.com>
2023-05-05 18:48:04 -0300

    chore: added tsConfigPaths in plugins

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-// import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-//  plugins: [tsconfigPaths()],
-  test: {
-    globals: true,
-  },
+    plugins: [tsconfigPaths()],
+    test: {
+        globals: true,
+    },
 });
