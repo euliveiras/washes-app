990f2b2f58225b13809be8232999772d6383472d
euliveiras <matheus.skm@hotmail.com>
2023-05-11 17:08:17 -0300

    refactor: removed proprietary factory

--- domain/modules/vehicle/entities/vehicle.spec.ts
+++ domain/modules/vehicle/entities/vehicle.spec.ts
@@ -2,7 +2,6 @@ import { describe, expect, it } from "vitest";
 import { Vehicle } from "./Vehicle";
 import { makeVehicle } from "test/factories/makeVehicle";
 import { makeDriver } from "test/factories/makeDriver";
-import { makeProprietary } from "test/factories/makeProprietary";
 
 describe("Vehicle", () => {
     it("should create a Vehicle", () => {
@@ -16,12 +15,5 @@ describe("Vehicle", () => {
         const vehicle = makeVehicle({ driverId: driver.id });
 
         expect(vehicle).toHaveProperty("driverId", driver.id);
-    });
-
-    it("should create a Vehicle with given proprietary", () => {
-        const proprietary = makeProprietary({ id: "123213asda"})
-        const vehicle = makeVehicle({ proprietaryId: proprietary.id});
-
-        expect(vehicle).toHaveProperty("proprietaryId", proprietary.id);
     });
 });
