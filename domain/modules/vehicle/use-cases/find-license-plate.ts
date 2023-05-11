7aaad3b3da195e90faf1aca72d7eaab9bead2335
euliveiras <matheus.skm@hotmail.com>
2023-05-11 20:05:30 -0300

    refactor: added execute argument type

--- domain/modules/vehicle/use-cases/find-license-plate.ts
+++ domain/modules/vehicle/use-cases/find-license-plate.ts
@@ -1,8 +1,11 @@
 import type { VehicleRepository } from "../repositories/vehicle-repository";
 
+type FindLicensePlateProps = {
+    plate: string;
+};
 export class FindLicensePlate {
     constructor(private db: VehicleRepository) {}
-    async execute(plate: string) {
+    async execute({ plate }: FindLicensePlateProps) {
         const vehicle = await this.db.findByLicensePlate(plate);
 
         return { vehicle };
