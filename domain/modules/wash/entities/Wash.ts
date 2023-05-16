9f08c2a8781c9316677616e12c8c28ed9ca459bd
euliveiras <matheus.skm@hotmail.com>
2023-05-16 15:47:25 -0300

    feat: created Wash entity

---
+++ domain/modules/wash/entities/Wash.ts
@@ -0,0 +1,57 @@
+import { randomUUID } from "crypto";
+
+export type WashProps = {
+    id: string;
+    vehicleId: string;
+    scheduleDate: string;
+    cycleId: string;
+    note: string;
+    isCompleted: boolean;
+};
+
+export class Wash {
+    private _props: WashProps;
+
+    constructor(props: WashProps) {
+        if (!props.id) {
+            props.id = randomUUID();
+        }
+        this._props = props;
+    }
+
+    public get id() {
+        return this._props.id;
+    }
+
+    public get vehicleId() {
+        return this._props.vehicleId;
+    }
+
+    public get scheduleDate() {
+        return this._props.scheduleDate;
+    }
+
+    public set scheduleDate(date: string) {
+        this._props.scheduleDate = date;
+    }
+
+    public get cycleId() {
+        return this._props.cycleId;
+    }
+
+    public get note() {
+        return this._props.note;
+    }
+
+    public set note(note: string) {
+        this._props.note = note;
+    }
+
+    public get isCompleted() {
+        return this._props.isCompleted;
+    }
+
+    public set isCompleted(bool: boolean) {
+        this._props.isCompleted = bool;
+    }
+}
