dd1177747f3b3ef1ee3b9becb19ff23c307f7125
euliveiras <matheus.skm@hotmail.com>
2023-05-16 11:14:01 -0300

    test: it should return true if a date is before another comparation date

--- domain/shared/date-manipulator.spec.ts
+++ domain/shared/date-manipulator.spec.ts
@@ -2,9 +2,14 @@ import { describe, expect, it } from "vitest";
 import { dateManipulator } from "./date-manipulator";
 
 describe("Date manipulation", () => {
-    it("should return true a date is after another comparation date", () => {
+    it("should return true if a date is after another comparation date", () => {
         const dateX = new Date(2023, 5, 15).toISOString();
         const dateY = new Date().toISOString();
         expect(dateManipulator.isAfter(dateX, dateY)).toBeTruthy();
+    });
+    it("should return true if a date is before another comparation date", () => {
+        const dateX = new Date(2023, 5, 15).toISOString();
+        const dateY = new Date().toISOString();
+        expect(dateManipulator.isBefore(dateY, dateX)).toBeTruthy();
     });
 });
