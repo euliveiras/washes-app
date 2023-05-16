73eb2b22cf6c2912c522974785d09ee130ba6523
euliveiras <matheus.skm@hotmail.com>
2023-05-16 16:27:23 -0300

    refactor: changed create argument type

--- domain/modules/wash-cycle/repositories/wash-cycle-repository.ts
+++ domain/modules/wash-cycle/repositories/wash-cycle-repository.ts
@@ -1,5 +1,6 @@
 import type { WashCycle } from "../entities/WashCycle";
+import type { InitializeCycleProps } from "../interfaces/initialize-cycle";
 
 export abstract class WashCycleRepository {
-    abstract create(cycle: WashCycle): Promise<WashCycle>;
+    abstract create(cycle: InitializeCycleProps): Promise<WashCycle>;
 }
