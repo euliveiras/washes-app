8dee93ed19ba18a086e2e0d58235fb077a4edefa
euliveiras <matheus.skm@hotmail.com>
2023-05-16 19:18:36 -0300

    feat: added washCycle repo in constructor

--- domain/modules/wash/use-cases/create-wash.ts
+++ domain/modules/wash/use-cases/create-wash.ts
@@ -1,11 +1,17 @@
+import type { WashCycleRepository } from "domain/modules/wash-cycle/repositories/wash-cycle-repository";
 import { Wash } from "../entities/Wash";
 import type { CreateWashProps } from "../interfaces/create-wash";
 import type { WashRepository } from "../repositories/wash-repository";
 
 export class CreateWash {
-    constructor(private db: WashRepository) {}
+    constructor(private washesRepo: WashRepository, private washesCycleRepo: WashCycleRepository) {}
     async execute(data: CreateWashProps): Promise<{ wash: Wash }> {
+        const findedWashCycle = await this.washesCycleRepo.find(data.cycleId);
+        if (!findedWashCycle) {
+            throw new Error("Wash cycle not founded");
+        }
         const wash = new Wash(data);
+        await this.washesRepo.create(wash);
         return { wash };
     }
 }
