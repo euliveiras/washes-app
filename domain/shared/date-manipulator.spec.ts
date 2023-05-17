e7c722166741d9a23dd7fda5b7570d6897f695ed
euliveiras <matheus.skm@hotmail.com>
2023-05-16 23:45:49 -0300

    refactor: some changes

--- domain/shared/date-manipulator.spec.ts
+++ domain/shared/date-manipulator.spec.ts
@@ -3,12 +3,12 @@ import { dateManipulator } from "./date-manipulator";
 
 describe("Date manipulation", () => {
     it("should return true if a date is after another comparation date", () => {
-        const dateX = new Date(2023, 5, 15).toISOString();
+        const dateX = new Date(2090, 4, 15).toISOString();
         const dateY = new Date().toISOString();
         expect(dateManipulator.isAfter(dateX, dateY)).toBeTruthy();
     });
     it("should return true if a date is before another comparation date", () => {
-        const dateX = new Date(2023, 5, 15).toISOString();
+        const dateX = new Date(2090, 5, 15).toISOString();
         const dateY = new Date().toISOString();
         expect(dateManipulator.isBefore(dateY, dateX)).toBeTruthy();
     });
--- test/database/in-memory-wash-cycle-repository.ts.ts
+++ test/database/in-memory-wash-cycle-repository.ts.ts
@@ -1,16 +1,13 @@
-import { WashCycle } from "domain/modules/wash-cycle/entities/WashCycle";
+import type { WashCycle } from "domain/modules/wash-cycle/entities/WashCycle";
 import type { WashCycleRepository } from "domain/modules/wash-cycle/repositories/wash-cycle-repository";
 
 export class InMemoryWashCycleRepository implements WashCycleRepository {
     private cycles: WashCycle[] = [];
-    async create(data: WashCycle): Promise<WashCycle> {
-        const washCycle = new WashCycle(data);
-        this.cycles.push(washCycle);
-
-        return washCycle;
+    async create(data: WashCycle): Promise<void> {
+        this.cycles.push(data);
     }
     async find(id: string): Promise<WashCycle | null> {
-        const washCycle = this.cycles.find(cycle => cycle.id === id)
-        return washCycle ?? null
+        const washCycle = this.cycles.find((cycle) => cycle.id === id);
+        return washCycle ?? null;
     }
 }
