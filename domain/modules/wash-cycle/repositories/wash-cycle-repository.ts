4c4589ead1fbca57df4d7e4b5c25ed5637fd2a4b
euliveiras <matheus.skm@hotmail.com>
2023-05-16 23:42:43 -0300

    refactor: changed create method return type

--- domain/modules/wash-cycle/repositories/wash-cycle-repository.ts
+++ domain/modules/wash-cycle/repositories/wash-cycle-repository.ts
@@ -1,6 +1,6 @@
 import type { WashCycle } from "../entities/WashCycle";
 
 export abstract class WashCycleRepository {
-    abstract create(cycle: WashCycle): Promise<WashCycle>;
+    abstract create(cycle: WashCycle): Promise<void>;
     abstract find(id: string): Promise<WashCycle | null>;
 }
