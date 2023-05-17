87e138ef8628bb5c9faef484e3d0cc94dcd8837c
euliveiras <matheus.skm@hotmail.com>
2023-05-17 10:49:44 -0300

    refactor: changed name to Organization

---
+++ test/factories/makeOrganization.ts
@@ -0,0 +1,14 @@
+import type { OrganizationProps } from "domain/modules/proprietary/entities/Organization";
+import { Organization } from "domain/modules/proprietary/entities/Organization";
+
+type OverrideProps = Partial<OrganizationProps>;
+
+export function makeOrganization(override?: OverrideProps) {
+    return new Organization({
+        id: "some-id",
+        name: "some-name",
+        phone: ["12313213"],
+        type: "some-type",
+        ...override,
+    });
+}
