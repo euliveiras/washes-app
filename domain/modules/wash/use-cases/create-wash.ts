45a7626884b47d6adf48dc4f823131790a3b7fc2
euliveiras <matheus.skm@hotmail.com>
2023-05-16 23:44:36 -0300

    feat: added date checks

--- domain/modules/wash/use-cases/create-wash.ts
+++ domain/modules/wash/use-cases/create-wash.ts
@@ -2,6 +2,7 @@ import type { WashCycleRepository } from "domain/modules/wash-cycle/repositories
 import { Wash } from "../entities/Wash";
 import type { CreateWashProps } from "../interfaces/create-wash";
 import type { WashRepository } from "../repositories/wash-repository";
+import { dateManipulator } from "domain/shared/date-manipulator";
 
 export class CreateWash {
     constructor(private washesRepo: WashRepository, private washesCycleRepo: WashCycleRepository) {}
@@ -10,8 +11,29 @@ export class CreateWash {
         if (!findedWashCycle) {
             throw new Error("Wash cycle not founded");
         }
+
+        const isScheduledDateBeforeCycleStart = dateManipulator.isBefore(
+            data.scheduleDate,
+            findedWashCycle.startDate
+        );
+
+        if (isScheduledDateBeforeCycleStart) {
+            throw new Error("Wash schedule date must be after wash cycle start date");
+        }
+
+        const isScheduledDateAfterCycleEnd = dateManipulator.isAfter(
+            data.scheduleDate,
+            findedWashCycle.endDate
+        );
+
+        if (isScheduledDateAfterCycleEnd) {
+            throw new Error("Wash schedule date must be before wash cycle end date");
+        }
+
         const wash = new Wash(data);
+
         await this.washesRepo.create(wash);
+
         return { wash };
     }
 }
