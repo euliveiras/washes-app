711627488020b5af79a9f44fb5279b0d89372943
euliveiras <matheus.skm@hotmail.com>
2023-05-17 11:53:17 -0300

    refactor: removed class constructor

--- test/database/in-memory-driver-repository.ts
+++ test/database/in-memory-driver-repository.ts
@@ -1,10 +1,9 @@
-import { Driver } from "domain/modules/driver/entities/Driver";
+import type { Driver } from "domain/modules/driver/entities/Driver";
 import type { DriverRepository } from "domain/modules/driver/repositories/driver-repository";
 
 export class InMemoryDriverRepository implements DriverRepository {
     private drivers: Driver[] = [];
-    async create(data: Driver): Promise<Driver> {
-        const driver = new Driver(data);
+    async create(driver: Driver): Promise<Driver> {
         this.drivers.push(driver);
         return driver;
     }
