292d062f955d76dfdfcc7dce2bdaa21754edd918
euliveiras <matheus.skm@hotmail.com>
2023-05-18 16:32:45 -0300

    test: removed some tests

--- domain/modules/driver/entities/driver.spec.ts
+++ domain/modules/driver/entities/driver.spec.ts
@@ -1,5 +1,3 @@
-import { Driver } from "./Driver";
-import { makeVehicle } from "test/factories/makeVehicle";
 import { makeDriver } from "test/factories/makeDriver";
 import { describe, expect, it } from "vitest";
 
@@ -8,25 +6,5 @@ describe("Driver", () => {
         const driver = makeDriver();
 
         expect(driver).toHaveProperty("name", "John Doe");
-    });
-    it("should not create a Driver with minus than 3 characters in name", () => {
-        expect(
-            () =>
-                new Driver({
-                    id: "some-id",
-                    name: "as",
-                    phones: ["123213213"],
-                    vehicleIds: ["vehicle-id-1, vehicle-id-2"],
-                    organizationId: "some-id",
-                })
-        ).toThrow();
-    });
-    it("should create a driver with a given vehicle id", () => {
-        const driver = makeDriver({ id: "john-doe-id" });
-        const vehicle = makeVehicle({ id: "custom-id" });
-
-        driver.addVehicleId(vehicle.id || "");
-
-        expect(driver.vehicleIds).toContain(vehicle.id);
     });
 });
