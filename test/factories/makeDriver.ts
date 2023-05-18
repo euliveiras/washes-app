1295894614fdae241fc5da67f0d9fc4c3a2c170c
euliveiras <matheus.skm@hotmail.com>
2023-05-18 16:32:09 -0300

    refactor: removed some props from driver factory

--- test/factories/makeDriver.ts
+++ test/factories/makeDriver.ts
@@ -5,10 +5,8 @@ type OverrideProps = Partial<DriverProps>;
 
 export function makeDriver(override?: OverrideProps) {
     return new Driver({
-        id: "some-id",
         name: "John Doe",
         phones: ["12313131"],
-        vehicleIds: ["some-id"],
         organizationId: "some-organization",
         ...override,
     });
