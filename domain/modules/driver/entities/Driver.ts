8305c6f15f19300932efa2748573f34d9fff8c2c
euliveiras <matheus.skm@hotmail.com>
2023-05-18 16:31:00 -0300

    refactor: removed some props from Driver

--- domain/modules/driver/entities/Driver.ts
+++ domain/modules/driver/entities/Driver.ts
@@ -1,11 +1,7 @@
-import { randomUUID } from "crypto";
-
 export type Replace<T, R> = Omit<T, keyof R> & R;
 
 export type DriverProps = {
-    id: string;
     name: string;
-    vehicleIds: string[];
     phones: string[];
     organizationId?: string;
 };
@@ -15,33 +11,10 @@ export class Driver {
     constructor(
         props: Replace<DriverProps, { id?: string; vehicleIds?: string[]; phones?: string[] }>
     ) {
-        this.validateName(props.name);
         this._props = {
             ...props,
-            vehicleIds: props.vehicleIds ?? [],
             phones: props.phones ?? [],
-            id: props.id ?? randomUUID(),
         };
-    }
-
-    private validateName(name: string) {
-        if (name.length < 3) {
-            throw new Error("Name must have more than 2 words");
-        }
-
-        return true;
-    }
-
-    public addPhoneNumber(num: string) {
-        this._props.phones.push(num);
-    }
-
-    public addVehicleId(id: string) {
-        this._props.vehicleIds.push(id);
-    }
-
-    public get id() {
-        return this._props.id;
     }
 
     public get name() {
@@ -50,10 +23,6 @@ export class Driver {
 
     private set name(name) {
         this._props.name = name;
-    }
-
-    public get vehicleIds() {
-        return this._props.vehicleIds;
     }
 
     public get phones() {
