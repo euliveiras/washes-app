2e7e515a020b9d90629d26a5337a63dace229551
euliveiras <matheus.skm@hotmail.com>
2023-05-18 17:18:24 -0300

    refactor: added vehicle instantiation

--- domain/modules/vehicle/use-cases/create-vehicle.ts
+++ domain/modules/vehicle/use-cases/create-vehicle.ts
@@ -1,10 +1,16 @@
+import { Driver } from "domain/modules/driver/entities/Driver";
+import { Vehicle } from "../entities/Vehicle";
 import type { CreateVehicleProps } from "../interfaces/create-vehicle";
 import type { VehicleRepository } from "../repositories/vehicle-repository";
 
 export class CreateVehicle {
     constructor(private db: VehicleRepository) {}
     async execute(data: CreateVehicleProps) {
-        const vehicle = await this.db.create(data);
+        const vehicle = new Vehicle({
+            ...data,
+            driver: new Driver({ ...data.driver }),
+        });
+        await this.db.create(vehicle);
         return { vehicle };
     }
 }
