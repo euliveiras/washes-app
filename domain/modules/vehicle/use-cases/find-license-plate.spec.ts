4b7bdd02367a832a937ebe29f3906c573822f206
euliveiras <matheus.skm@hotmail.com>
2023-05-18 17:18:51 -0300

    refactor: removed unecessary instantiation

--- domain/modules/vehicle/use-cases/find-license-plate.spec.ts
+++ domain/modules/vehicle/use-cases/find-license-plate.spec.ts
@@ -3,7 +3,7 @@ import { CreateVehicle } from "./create-vehicle";
 import { InMemoryVehicleRepository } from "test/database/in-memory-vehicle-repository";
 import { FindLicensePlate } from "./find-license-plate";
 import { Vehicle } from "../entities/Vehicle";
-import { makeVehicle } from "test/factories/makeVehicle";
+import { makeDriver } from "test/factories/makeDriver";
 
 const inMemoryVehicleRepository = new InMemoryVehicleRepository();
 const createVehicle = new CreateVehicle(inMemoryVehicleRepository);
@@ -12,12 +12,11 @@ const findLicensePlate = new FindLicensePlate(inMemoryVehicleRepository);
 describe("Find license plate", () => {
     it("should find vehicle by a given license plate", async () => {
         const licensePlate = "123KASD";
-        const mockedVehicle = makeVehicle({ licensePlate });
 
-        await createVehicle.execute(mockedVehicle);
+        await createVehicle.execute({ licensePlate, driver: makeDriver(), vehicleType: "Bitruck" });
 
         const { vehicle } = await findLicensePlate.execute({ plate: licensePlate });
-        
+
         expect(vehicle).toBeInstanceOf(Vehicle);
     });
 });
