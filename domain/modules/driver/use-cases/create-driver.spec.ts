098d82bf3a4cdd67ef8644b5273a0a8055875f04
euliveiras <matheus.skm@hotmail.com>
2023-05-18 16:35:48 -0300

    test: removed repository instantiation

--- domain/modules/driver/use-cases/create-driver.spec.ts
+++ domain/modules/driver/use-cases/create-driver.spec.ts
@@ -1,13 +1,11 @@
 import { describe, expect, it } from "vitest";
 import { CreateDriver } from "./create-driver";
 import { Driver } from "../entities/Driver";
-import { InMemoryDriverRepository } from "test/database/in-memory-driver-repository";
 import { makeDriver } from "test/factories/makeDriver";
 
 describe("Create driver", () => {
     it("should create a driver", async () => {
-        const inMemoryDriverRepository = new InMemoryDriverRepository();
-        const createDriver = new CreateDriver(inMemoryDriverRepository);
+        const createDriver = new CreateDriver();
 
         const { driver } = await createDriver.execute(makeDriver());
         expect(driver).toBeInstanceOf(Driver);
