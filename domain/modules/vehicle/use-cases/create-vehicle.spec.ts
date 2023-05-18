15ac6c521ab5cbe1837929c0183a6f60f5333672
euliveiras <matheus.skm@hotmail.com>
2023-05-18 17:17:30 -0300

    refactor: added makeDriver to driver prop

--- domain/modules/vehicle/use-cases/create-vehicle.spec.ts
+++ domain/modules/vehicle/use-cases/create-vehicle.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { CreateVehicle } from "./create-vehicle";
 import { InMemoryVehicleRepository } from "test/database/in-memory-vehicle-repository";
+import { makeDriver } from "test/factories/makeDriver";
 
 const inMemoryVehicleRepository = new InMemoryVehicleRepository();
 const createVehicle = new CreateVehicle(inMemoryVehicleRepository);
@@ -10,6 +11,7 @@ describe("Create vehicle", () => {
         const { vehicle } = await createVehicle.execute({
             licensePlate: "1239ASD",
             vehicleType: "Bitruck",
+            driver: makeDriver(),
         });
         expect(vehicle).toHaveProperty("licensePlate");
     });
