a3e0c450eb7251bf6e5f721ea7b47308f3973b3d
euliveiras <matheus.skm@hotmail.com>
2023-05-19 09:10:04 -0300

    feat: created findNextCycleByLicensePlate repo method

--- domain/modules/wash-cycle/repositories/wash-cycle-repository.ts
+++ domain/modules/wash-cycle/repositories/wash-cycle-repository.ts
@@ -3,4 +3,5 @@ import type { WashCycle } from "../entities/WashCycle";
 export abstract class WashCycleRepository {
     abstract create(cycle: WashCycle): Promise<void>;
     abstract find(id: string): Promise<WashCycle | null>;
+    abstract findNextCycleByLicensePlate(id: string): Promise<WashCycle | null>;
 }
