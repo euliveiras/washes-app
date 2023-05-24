ec6a41f31a8bc4a1d7d6b745bc19f6d092e1f687
euliveiras <matheus.skm@hotmail.com>
2023-05-24 16:01:10 -0300

    feat: created find method

--- domain/modules/wash/repositories/wash-repository.ts
+++ domain/modules/wash/repositories/wash-repository.ts
@@ -2,4 +2,5 @@ import type { Wash } from "../entities/Wash";
 
 export abstract class WashRepository {
     abstract create(wash: Wash): Promise<Wash>;
+    abstract find(id: string): Promise<Wash | null>;
 }
