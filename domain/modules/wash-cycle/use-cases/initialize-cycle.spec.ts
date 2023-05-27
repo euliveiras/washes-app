cebf5f9aae664fb29ed6e47c0f6d383aeb257f4d
euliveiras <matheus.skm@hotmail.com>
2023-05-27 20:51:10 -0300

    test: it should initialize a cycle with four washes

--- domain/modules/wash-cycle/use-cases/initialize-cycle.spec.ts
+++ domain/modules/wash-cycle/use-cases/initialize-cycle.spec.ts
@@ -1,11 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { InitializeCycle } from "./initialize-cycle";
 import { InMemoryWashCycleRepository } from "test/database/in-memory-wash-cycle-repository.ts";
+import { dateManipulator } from "domain/shared/date-manipulator";
+import { Wash } from "domain/modules/wash/entities/Wash";
+import { FindWashById } from "domain/modules/wash/use-cases/find-wash-by-id";
+import { InMemoryWashRepository } from "test/database/in-memory-wash-repository";
 
 describe("New wash", () => {
     it("should initialize a wash cycle", async () => {
         const inMemoryWashCycleRepo = new InMemoryWashCycleRepository();
-        const initializeWash = new InitializeCycle(inMemoryWashCycleRepo);
+        const inMemoryWashRepo = new InMemoryWashRepository();
+        const initializeWash = new InitializeCycle(inMemoryWashCycleRepo, inMemoryWashRepo);
         const { washCycle } = await initializeWash.execute({
             endDate: new Date(2024, 1, 1).toISOString(),
             startDate: new Date().toISOString(),
@@ -13,5 +18,24 @@ describe("New wash", () => {
         });
 
         expect(washCycle).toBeTruthy();
+    });
+    it("should initialize a wash cycle with four washes", async () => {
+        const inMemoryWashCycleRepo = new InMemoryWashCycleRepository();
+        const inMemoryWashRepo = new InMemoryWashRepository();
+        const initializeWash = new InitializeCycle(inMemoryWashCycleRepo, inMemoryWashRepo);
+        const startDate = dateManipulator.parseDateToString(new Date());
+        const endDate = dateManipulator.addMonthsToDate(startDate, 1);
+        const { washCycle } = await initializeWash.execute({
+            endDate,
+            startDate,
+            vehicleId: "some-vehicle-id",
+            washes: [
+                { scheduledDate: startDate, isCompleted: true },
+                { scheduledDate: dateManipulator.addDaysToDate(startDate, 7) },
+                { scheduledDate: dateManipulator.addDaysToDate(startDate, 14) },
+                { scheduledDate: dateManipulator.addDaysToDate(startDate, 21) },
+            ],
+        });
+        expect(washCycle.washesId).toHaveLength(4);
     });
 });
