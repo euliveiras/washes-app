c4f94ad5ac8d7dab8556f74103da07dcab066616
euliveiras <matheus.skm@hotmail.com>
2023-05-27 20:51:43 -0300

    refactor: added 4 washes creation

--- domain/modules/wash-cycle/use-cases/initialize-cycle.ts
+++ domain/modules/wash-cycle/use-cases/initialize-cycle.ts
@@ -1,11 +1,34 @@
+import { WashRepository } from "domain/modules/wash/repositories/wash-repository";
 import { WashCycle } from "../entities/WashCycle";
 import type { InitializeCycleProps } from "../interfaces/initialize-cycle";
 import type { WashCycleRepository } from "../repositories/wash-cycle-repository";
+import { Wash } from "domain/modules/wash/entities/Wash";
 
 export class InitializeCycle {
-    constructor(private db: WashCycleRepository) {}
-    async execute(data: InitializeCycleProps): Promise<{ washCycle: WashCycle }> {
-        const washCycle = new WashCycle(data)
+    constructor(private db: WashCycleRepository, private washRepository: WashRepository) {}
+    async execute({
+        endDate,
+        startDate,
+        vehicleId,
+        note,
+        washes: washesData,
+    }: InitializeCycleProps): Promise<{ washCycle: WashCycle }> {
+        const washCycle = new WashCycle({ endDate, startDate, vehicleId, note });
+
+        if (washesData) {
+            await Promise.all(
+                washesData.map(async (w) => {
+                    const wash = new Wash({
+                        scheduleDate: w.scheduledDate,
+                        cycleId: washCycle.id,
+                        vehicleId,
+                        isCompleted: w.isCompleted ?? false,
+                    });
+                    await this.washRepository.create(wash);
+                    washCycle.addWashId(wash.id);
+                })
+            );
+        }
         await this.db.create(washCycle);
         return { washCycle };
     }
