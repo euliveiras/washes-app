2cd6b16b6b8b7fb3f87142c6aae3cd75abaebb43
euliveiras <matheus.skm@hotmail.com>
2023-05-28 00:07:57 -0300

    refactor: changed update argument method type

--- domain/modules/wash/repositories/wash-repository.ts
+++ domain/modules/wash/repositories/wash-repository.ts
@@ -3,4 +3,5 @@ import type { Wash } from "../entities/Wash";
 export abstract class WashRepository {
     abstract create(wash: Wash): Promise<Wash>;
     abstract findById(id: string): Promise<Wash | null>;
+    abstract update(id: string, data: Partial<Wash>): Promise<Wash | null>;
 }
