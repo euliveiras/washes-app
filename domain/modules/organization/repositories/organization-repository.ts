39ccefa252dcec91fc64675e32a8f1fd4646f1c0
euliveiras <matheus.skm@hotmail.com>
2023-06-15 20:38:50 -0300

    test: implemented findByName method

--- domain/modules/organization/repositories/organization-repository.ts
+++ domain/modules/organization/repositories/organization-repository.ts
@@ -2,4 +2,5 @@ import type { Organization } from "../entities/Organization";
 
 export abstract class OrganizationRepository {
     abstract create(cycle: Organization): Promise<void>;
+    abstract findByName(name: string): Promise<Organization | null>;
 }
