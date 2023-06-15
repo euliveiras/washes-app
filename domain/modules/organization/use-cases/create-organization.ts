9c729007d3986296d6530cf98a2f827fde15c731
euliveiras <matheus.skm@hotmail.com>
2023-06-15 20:07:49 -0300

    feat: created CreateOrganization use-case

---
+++ domain/modules/organization/use-cases/create-organization.ts
@@ -0,0 +1,17 @@
+import { Organization } from "../entities/Organization";
+import type { OrganizationRepository } from "../repositories/organization-repository";
+
+type CreateOrganizationDTO = {
+    type: "Cooperativa" | "Empresa";
+    name: string;
+    phone: string[];
+};
+export class CreateOrganization {
+    constructor(private organizationDb: OrganizationRepository) {}
+
+    async execute(data: CreateOrganizationDTO): Promise<void> {
+        const organization = new Organization(data)
+
+        await this.organizationDb.create(organization)
+    }
+}
