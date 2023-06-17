ca8402bcd53b6da510f0f38ec7ed3120df666bc7
euliveiras <matheus.skm@hotmail.com>
2023-06-17 18:36:35 -0300

    feat: created abstract method find

--- domain/modules/user/repositories/user-repository.ts
+++ domain/modules/user/repositories/user-repository.ts
@@ -2,4 +2,5 @@ import type { User } from "../entities/User";
 
 export abstract class UserRepository {
     abstract create(user: User): Promise<void>;
+    abstract find(query: { username?: string; id?: string; email?: string }): Promise<User | null>;
 }
