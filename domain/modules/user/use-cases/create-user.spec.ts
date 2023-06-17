09adde2841ba3d7df7aaba8eea060581e75c79ea
euliveiras <matheus.skm@hotmail.com>
2023-06-17 17:34:18 -0300

    it: should create a user

---
+++ domain/modules/user/use-cases/create-user.spec.ts
@@ -0,0 +1,20 @@
+import { expect, it, describe } from "vitest";
+import { CreateUser } from "./create-user";
+import { InMemoryUserRepository } from "test/database/in-memory-user-repository";
+import { User } from "../entities/User";
+
+describe("Create user", () => {
+    it("should create a user", async () => {
+        const inMemoryUserRepository = new InMemoryUserRepository();
+        const createUser = new CreateUser(inMemoryUserRepository);
+
+        const { user } = await createUser.execute({
+            username: "euliveiras",
+            password: "123456",
+            email: "matheus.skm@hotmail.com",
+            role: "ADMIN",
+        });
+
+        expect(user).toBeInstanceOf(User);
+    });
+});
