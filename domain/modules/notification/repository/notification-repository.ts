4f097886d186adf21c4bf380137b0345fd447ccf
euliveiras <matheus.skm@hotmail.com>
2023-06-19 19:31:48 -0300

    refactor: changed query types

--- domain/modules/notification/repository/notification-repository.ts
+++ domain/modules/notification/repository/notification-repository.ts
@@ -4,10 +4,10 @@ export abstract class NotificationRepository {
     abstract create(notification: Notification): Promise<void>;
     abstract findMany(query: {
         where: {
-            recipientId: string;
-            date: string;
+            recipientId?: string;
+            date?: string;
         };
-    }): Promise<Notification[] | null>;
+    }): Promise<Notification[]>;
     abstract updateMany(
         query: {
             where: { recipientId: string; date: string };
