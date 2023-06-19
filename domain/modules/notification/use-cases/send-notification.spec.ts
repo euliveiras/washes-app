657b7a5f11dc4599c33da1b1b0a2c68f93d37d64
euliveiras <matheus.skm@hotmail.com>
2023-06-19 17:42:16 -0300

    test: it should send a notification

---
+++ domain/modules/notification/use-cases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { expect, it, describe } from "vitest";
+import { SendNotification } from "./send-notification";
+import { InMemoryNotificationRepository } from "test/database/in-memory-notification-repository";
+
+describe("Send notification", () => {
+    it("should send a notification", async () => {
+        const inMemoryNotificationRepo = new InMemoryNotificationRepository();
+        const sendNotification = new SendNotification(inMemoryNotificationRepo);
+
+        await expect(
+            sendNotification.execute({
+                date: new Date(),
+                content: "some-content",
+                recipientId: "some-id",
+            })
+        ).resolves.toBeDefined();
+    });
+});
