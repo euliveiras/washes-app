b392de32f2f40527919afb050acd2ee9a8e5f3a4
euliveiras <matheus.skm@hotmail.com>
2023-06-19 17:32:39 -0300

    feat: created send-notification use-case

---
+++ domain/modules/notification/use-cases/send-notification.ts
@@ -0,0 +1,20 @@
+import type { NotificationRepository } from "../repository/notification-repository";
+import { Notification } from "../entity/Notification";
+
+type SendNotificationProps = {
+    date: string;
+    recipientId: string;
+    content: string;
+};
+
+export class SendNotification {
+    constructor(private notificationDb: NotificationRepository) {}
+
+    async execute(data: SendNotificationProps): Promise<{ notification: Notification }> {
+        const notification = new Notification(data);
+
+        await this.notificationDb.create(notification);
+
+        return { notification };
+    }
+}
