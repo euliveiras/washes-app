064f19419ced450cc2a2c28cb6d6bdbcbbcabdff
euliveiras <matheus.skm@hotmail.com>
2023-06-19 17:32:01 -0300

    test: removed toISOString call

--- domain/modules/notification/entity/notification.spec.ts
+++ domain/modules/notification/entity/notification.spec.ts
@@ -6,7 +6,7 @@ describe("Notification entity", () => {
         const notification = new Notification({
             content: "Lavagem hoje, quarta-feira, às 4 da tarde",
             recipientId: "fake-id",
-            date: new Date().toISOString(),
+            date: new Date(),
         });
 
         expect(notification).toBeInstanceOf(Notification);
