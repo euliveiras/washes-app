6f326fed987e85554b040a30b6e213ceafeec868
euliveiras <matheus.skm@hotmail.com>
2023-06-19 16:50:59 -0300

    refactor: added find and update methods in repo class

--- domain/modules/notification/repository/notification-repository.ts
+++ domain/modules/notification/repository/notification-repository.ts
@@ -1,3 +1,15 @@
 export abstract class NotificationRepository {
     abstract create(notification: Notification): Promise<void>;
+    abstract findMany(query: {
+        where: {
+            recipientId: string;
+            date: string;
+        };
+    }): Promise<Notification[] | null>;
+    abstract updateMany(
+        query: {
+            where: { recipientId: string; date: string };
+        },
+        data: Partial<Notification>
+    ): Promise<void>;
 }
