7887d39cc76b3c2014a51364eeb23ebd1a5a19be
euliveiras <matheus.skm@hotmail.com>
2023-06-19 20:05:46 -0300

    test: it should mark notification as readed

---
+++ domain/modules/notification/use-cases/read-notification.spec.ts
@@ -0,0 +1,25 @@
+import { expect, it, describe } from "vitest";
+import { ReadNotification } from "./read-notification";
+import { InMemoryNotificationRepository } from "test/database/in-memory-notification-repository";
+import { SendNotification } from "./send-notification";
+import { makeNotification } from "test/factories/makeNotification";
+
+describe("Send notification", () => {
+    it("should send a notification", async () => {
+        const inMemoryNotificationRepo = new InMemoryNotificationRepository();
+        const sendNotification = new SendNotification(inMemoryNotificationRepo);
+        const readNotification = new ReadNotification(inMemoryNotificationRepo);
+
+        await sendNotification.execute(makeNotification({ date: new Date(2019, 3, 9) }));
+        await sendNotification.execute(makeNotification({ date: new Date(2019, 4, 6) }));
+        const { notification } = await sendNotification.execute(
+            makeNotification({ date: new Date(2020, 2, 3) })
+        );
+
+        const { notification: readedNotification } = await readNotification.execute({
+            id: notification.id,
+        });
+
+        expect(readedNotification).toHaveProperty("readAt");
+    });
+});
