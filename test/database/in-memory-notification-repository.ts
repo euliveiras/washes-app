ae6328e43f66023a55f4c69f84055cf029ae615c
euliveiras <matheus.skm@hotmail.com>
2023-06-19 20:04:21 -0300

    test: implemented findByIdAndUpdate in memory repo method

--- test/database/in-memory-notification-repository.ts
+++ test/database/in-memory-notification-repository.ts
@@ -1,4 +1,4 @@
-import type { Notification } from "domain/modules/notification/entity/Notification";
+import { Notification } from "domain/modules/notification/entity/Notification";
 import type { NotificationRepository } from "domain/modules/notification/repository/notification-repository";
 
 export class InMemoryNotificationRepository implements NotificationRepository {
@@ -6,6 +6,7 @@ export class InMemoryNotificationRepository implements NotificationRepository {
     async create(notification: Notification): Promise<void> {
         this.data.push(notification);
     }
+
     async findMany(query: {
         where: { recipientId: string; date: string };
     }): Promise<Notification[]> {
@@ -20,10 +21,37 @@ export class InMemoryNotificationRepository implements NotificationRepository {
         });
         return notifications ?? [];
     }
+
+    async findByIdAndUpdate(id: string, data: Partial<Notification>): Promise<Notification> {
+        const index = this.data.findIndex((notification) => notification.id === id);
+
+        if (index < 0) {
+            throw new Error("Notification not founded");
+        }
+
+        const notification = this.data[index];
+
+        const updatedNotification = new Notification({
+            id,
+            content: notification?.content,
+            date: notification?.date,
+            recipientId: notification?.recipientId,
+            createdAt: notification?.createdAt,
+            readAt: notification?.readAt,
+            ...data,
+        });
+
+        this.data[index] = updatedNotification;
+
+        console.log(this.data)
+
+        return updatedNotification;
+    }
+
     updateMany(
-        query: { where: { recipientId: string; date: string } },
+        query: { where: { id: string; recipientId: string; date: string } },
         data: Partial<Notification>
-    ): Promise<void> {
+    ): Promise<Notification> {
         throw new Error("Method not implemented.");
     }
 }
