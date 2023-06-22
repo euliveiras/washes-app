df7df92088cfdeff362141617ee11f652796ddc3
euliveiras <matheus.skm@hotmail.com>
2023-06-22 10:36:06 -0300

    refactor: added user in return value

--- src/infra/http/controllers/sign-in-user-controller.ts
+++ src/infra/http/controllers/sign-in-user-controller.ts
@@ -9,9 +9,9 @@ export async function signInUserController({ email, password }: SignInUserContro
     try {
         const prismaUserRepository = new PrismaUserRepository();
         const createSession = new CreateSession(prismaUserRepository);
-        const { token } = await createSession.execute({ email, password });
+        const { token, user } = await createSession.execute({ email, password });
 
-        return { token };
+        return { token, user };
     } catch (err: unknown) {
         if (err instanceof Error) {
             return { error: err.message };
