544f1c78e0816ea968512f320c8224355e7af5d6
euliveiras <matheus.skm@hotmail.com>
2023-06-21 23:06:33 -0300

    refactor: added credentials checks

--- src/domain/modules/user/use-cases/create-session.ts
+++ src/domain/modules/user/use-cases/create-session.ts
@@ -1,10 +1,34 @@
+import { hashManipulator } from "domain/shared/utils/hash-manipulator";
 import type { User } from "../entities/User";
 import type { UserRepository } from "../repositories/user-repository";
 
 export class CreateSession {
     constructor(private userDB: UserRepository) {}
 
-    async execute(user: User): Promise<{ token: string }> {
+    async execute({
+        email,
+        password,
+    }: {
+        email: string;
+        password: string;
+    }): Promise<{ token: string }> {
+        const user = await this.userDB.find({ email });
+
+        if (!user) {
+            console.log("user not founded");
+            throw new Error("Credentials not valid");
+        }
+
+        const isPasswordValid = await hashManipulator.compareStrToHashedStr(
+            password,
+            user.password
+        );
+
+        if (!isPasswordValid) {
+            console.log("password not valid");
+            throw new Error("Credentials not valid");
+        }
+
         const token = user.createSession();
 
         await this.userDB.update(user.id, { sessions: user.sessions });
