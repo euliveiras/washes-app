3643d702823000f014b39e5ae35b194de68c87db
euliveiras <matheus.skm@hotmail.com>
2023-07-06 18:25:02 -0300

    refactor: removed root page

--- app/routes/_index.tsx
+++
@@ -1,9 +0,0 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-
-export const meta: V2_MetaFunction = () => {
-    return [{ title: "New Remix App" }];
-};
-
-export default function Index() {
-    return <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>hello</div>;
-}
