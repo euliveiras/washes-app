9c97f8d9bb2138d1df150f557cbde88dd2da601a
matheus <matheus.skm@hotmail.com>
2023-08-16 16:53:12 -0300

    feat: created wash component

---
+++ app/components/WashBox/index.tsx
@@ -0,0 +1,26 @@
+import { LinkBox } from "~/components/LinkBox";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { Text } from "@chakra-ui/react";
+import { dateManipulator } from "domain/shared/date-manipulator";
+
+type WashBoxProps = {
+  wash: {
+    id: string;
+    scheduleDate: string;
+  };
+};
+
+export function WashBox({ wash }: WashBoxProps) {
+  const title = dateManipulator.format(wash.scheduleDate, "d/LL");
+  const date = dateManipulator.format(wash.scheduleDate, "d 'de' MMMM, eeee");
+
+  return (
+    <LinkBox.Container to={`/wash/${wash.id}`}>
+      <LinkBox.Content>
+        <LinkBox.Title>{title}</LinkBox.Title>
+        <Text fontSize={"lg"}>{date}</Text>
+      </LinkBox.Content>
+      <LinkBox.Icon icon={<FaExternalLinkAlt size={16} />} />
+    </LinkBox.Container>
+  );
+}
