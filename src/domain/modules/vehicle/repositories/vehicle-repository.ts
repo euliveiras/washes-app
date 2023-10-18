2b1cc1191d16b45b693e2fd01e3b8b3ba24c6fc6
Matheus Oliveira <matheus.skm@hotmail.com>
2023-10-18 19:28:28 -0300

    feat: created update asbtract method

--- src/domain/modules/vehicle/repositories/vehicle-repository.ts
+++ src/domain/modules/vehicle/repositories/vehicle-repository.ts
@@ -4,4 +4,5 @@ export abstract class VehicleRepository {
   abstract create(vehicle: Vehicle): Promise<void>;
   abstract findByLicensePlate(plate: string): Promise<Vehicle | null>;
   abstract findManyLicensePlate(str: string): Promise<Vehicle[]>;
+  abstract update(id: string, data: Partial<Vehicle>): Promise<void>;
 }
